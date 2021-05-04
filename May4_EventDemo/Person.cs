8daba6d734e90889226074435fdd362ed71eba4d
gvanbo <gvanbo@gmail.com>
2021-05-04 09:49:31 -0600

    Add project files.

---
+++ May4_EventDemo/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace May4_EventDemo
+{
+    class Person
+    {
+        public string FName { get; set; }
+
+        public string LName { get; set; }
+        private DateTime _dob;
+
+        public DateTime DOB {
+            get { return _dob; }
+            set { 
+                _dob = value;
+                DOBEvent(this); //Fire the Event
+            }
+        }
+
+
+        public delegate void DOBDelegate(Person p);
+
+        public event DOBDelegate DOBEvent;
+
+
+    }
+}
---
+++ May4_EventDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace May4_EventDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Person myPerson = new Person() { FName = "Mickey", LName = "Mouse" };
+            Console.WriteLine($"Created a new Person: {myPerson.FName} {myPerson.LName}");
+            myPerson.DOBEvent += DOB_Handle;
+            myPerson.DOBEvent += DOB_Handle2; //wire second event
+
+            myPerson.DOB = new DateTime(1925, 5, 05);
+
+        }
+
+
+
+
+        //Created Event Handler
+        static void DOB_Handle(Person p)
+        {
+            int days = DateTime.Today.DayOfYear - p.DOB.DayOfYear;
+
+            if (days > 0) Console.WriteLine($"Sorry, we missed your birthday.");
+            if (days < 0) Console.WriteLine($"{p.FName} {p.LName}, Your birthday is in {Math.Abs(days)} days.");
+            else Console.WriteLine($"Happy Birthday {p.FName} {p.LName}");
+
+        }
+
+        static void DOB_Handle2(Person p)
+        {
+            int days = DateTime.Today.DayOfYear - p.DOB.DayOfYear;
+
+            if (days > 0) Console.WriteLine($"Your Birthday was {days} ago.");
+            if (days < 0) Console.WriteLine($"Its true, Your birthday is in {Math.Abs(days)}.");
+            else Console.WriteLine("Happy Birthday");
+
+        }
+    }
+}
